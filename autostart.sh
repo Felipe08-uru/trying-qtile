15776a4e9b569cc2ab5bcde1296f4c8b70e3baf4
Felipe Bellas <felipebellas08@gmail.com>
2023-01-09 11:22:23 -0300

    first commit

---
+++ autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+blueman-applet &
+redshift -l -6.21462:106.84513 &
+
+~/.fehbg &
+copyq &
+nm-applet &
+mictray &
+pasystray &
+deadd-notification-center &
+nitrogen --restore &
+picom &
